f4fa8106ad146f05a02fb8b3912258071b36542c
kat coveney <katcoveney.author@gmail.com>
2024-06-13 11:48:14 -0500

    Create Database and added HTML to index

---
+++ scripts/database.js
@@ -0,0 +1,41 @@
+const database =[
+    areas = [{
+        id:1,
+        name:"Chamfort River",
+        activityId:1
+    },
+    {
+        id:2,
+        name:"Lost Wolf Hiking Trail",
+        activityId:2
+    },
+    {
+        id:3,
+        name:"Lodge",
+        activityId:3
+    },
+    {
+        id:4,
+        name:"Gander River",
+        activityId:4
+    },
+    {
+        id:5,
+        name:"Campgrounds",
+        activityId:5
+    },
+    {
+        id:6,
+        name:"Pine Bluffs Trails",
+        activityId:6
+    },
+    ],
+    
+    services = [
+
+    ],
+    guests =[
+        
+    ]
+    
+]
