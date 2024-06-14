74090e72b0969fc3963b5a9bc3628b7c1231b0b0
kat coveney <katcoveney.author@gmail.com>
2024-06-14 09:25:50 -0500

    added css to activites and created functionality for park areas

--- scripts/parkActivity.js
+++ scripts/parkActivity.js
@@ -5,7 +5,7 @@ const parkActivities = getActivities();
 export const ActivitiesHTML = () => {
   let html = "<ul>";
   for (const activity of parkActivities) {
-    html += `<li>${activity.name}</li>`;
+    html += `<li class = "activity">${activity.name}</li>`;
   }
   html += "</ul>";
   return html;
